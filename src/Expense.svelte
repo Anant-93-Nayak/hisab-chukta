c216795757f467e8689da1377be331543250c87b
“Anant-93-Nayak” <anantnayak93@gmail.com>
2020-10-21 14:49:47 +0530

    initial commit

---
+++ src/Expense.svelte
@@ -0,0 +1,37 @@
+<script>
+  import { getContext } from 'svelte'
+  import { slide } from 'svelte/transition'
+  export let id
+  export let name = ''
+  export let amount = 0
+  let displayAmount = false
+  function toggleAmount() {
+    displayAmount = !displayAmount
+  }
+  const removeExpense = getContext('remove')
+  const setModifiedExpense = getContext('modify')
+</script>
+
+<article class="single-expense">
+  <div class="expense-info">
+    <h2>
+      {name}
+      <button class="amount-btn" on:click|once={toggleAmount}>
+        <i class="fas fa-caret-down" />
+      </button>
+    </h2>
+    {#if displayAmount}
+      <h4 transition:slide>amount : ₹{amount}</h4>
+    {/if}
+  </div>
+  <div class="expense-buttons">
+    <button
+      class="expense-btn edit-btn"
+      on:click={() => setModifiedExpense(id)}>
+      <i class="fas fa-pen" />
+    </button>
+    <button class="expense-btn delete-btn" on:click={() => removeExpense(id)}>
+      <i class="fas fa-trash" />
+    </button>
+  </div>
+</article>
